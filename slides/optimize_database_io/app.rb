957ecd204119b01f84f7c788d0d99eaf30f04c31
Bogdan Gusiev <agresso@gmail.com>
2017-03-03 14:43:53 +0200

    do

--- slides/optimize_database_io/app.rb
+++ slides/optimize_database_io/app.rb
@@ -4,5 +4,10 @@ require "sass"
 
 get '/', :provides => 'html' do
 
+  headers 'Access-Control-Allow-Origin' => 'http://lab.hakim.se'
   haml :index
+end
+
+get "/test", provides: 'html' do
+  haml :test
 end
